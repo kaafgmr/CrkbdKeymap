988a94ddb162a0bb643d1b076bb985a2a05e7dfb
Kelson Alan Alberti Filho <kelsonfilho7@hotmail.com>
2024-10-22 23:19:43 +0200

    fixed lighting and oled timeout

--- keymap.c
+++ keymap.c
@@ -1,24 +1,25 @@
 #include QMK_KEYBOARD_H
+#include "quantum/keymap_extras/keymap_spanish.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+       KC_TAB,    ES_Q,    ES_W,    ES_E,    ES_R,    ES_T,                         ES_Y,    ES_U,    ES_I,    ES_O,   ES_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      KC_LSFT,    ES_A,    ES_S,    ES_D,    ES_F,    ES_G,                         ES_H,    ES_J,    ES_K,    ES_L, ES_NTIL, ES_ACUT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+      KC_LCTL,    ES_Z,    ES_X,    ES_C,    ES_V,    ES_B,                         ES_N,    ES_M, ES_COMM,  ES_DOT, ES_MINS,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_RALT
+                                          KC_LGUI,   MO(1),  KC_SPC,     KC_ENT,   MO(2), KC_LALT
                                       //`--------------------------'  `--------------------------'
 
   ),
 
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
+       KC_TAB,    ES_1,    ES_2,    ES_3,    ES_4,    ES_5,                         ES_6,    ES_7,    ES_8,    ES_9,    ES_0,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      KC_LSFT, KC_CAPS, XXXXXXX, XXXXXXX,A(KC_F4),  KC_F11,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -28,11 +29,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+       KC_TAB, ES_EXLM, ES_DQUO, ES_BULT,  ES_DLR, ES_PERC,                      ES_AMPR, ES_SLSH, ES_LPRN, ES_RPRN, ES_EQL,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+      KC_LSFT, ES_PIPE,   ES_AT, ES_HASH, ES_TILD, ES_EURO,                       ES_NOT, ES_LBRC, ES_LCBR, ES_RCBR, ES_RBRC, ES_CCED,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+      KC_LCTL, ES_LABK, ES_QUOT, XXXXXXX, XXXXXXX, XXXXXXX,                      ES_QUES, XXXXXXX, XXXXXXX, XXXXXXX, ES_PLUS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(3),  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -53,25 +54,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef OLED_ENABLE
 
-static void oled_render_layer_state(void) {
-    oled_write(PSTR("Layer: "), false);
-    switch (get_highest_layer(layer_state)) {
-        case 0:
-            oled_write_ln(PSTR("QWERTY"), false);
-            break;
-        case 1:
-            oled_write_ln(PSTR("NUMBERS"), false);
-            break;
-        case 2:
-            oled_write_ln(PSTR("SIMBOLS"), false);
-            break;
-        case 3:
-            oled_write_ln(PSTR("LIGHTS"), false);
-            break;
-    }
+const char* get_current_layer(void)
+{
+	switch (get_highest_layer(layer_state)) 
+	{
+		case 0:
+			return "QWERTY";
+			break;
+		case 1:
+			return "NUMBERS";
+			break;
+		case 2:
+			return "SIMBOLS";
+			break;
+		case 3:
+			return "LIGHTS";
+			break;
+		default:
+			return "QWERTY";
+			break;
+	}
 }
 
-static void oled_render_wpm(void)
+void oled_render_layer_state(void) {
+    oled_write(PSTR("Layer: "), false);
+	oled_write_ln(get_current_layer(), false);
+}
+
+void oled_render_wpm(void)
 {
 	oled_write(PSTR("WPM: "),false);
 	oled_write_ln(get_u8_str(get_current_wpm(), ' '), false);
@@ -88,14 +98,86 @@ __attribute__((weak)) void oled_render_logo(void) {
     oled_write(crkbd_logo, sizeof(crkbd_logo));
 }
 
-bool oled_task_user(void) {
-	oled_set_cursor(0,0);
-    if (is_keyboard_left()) {
-        oled_render_layer_state();
+bool oled_task_user(void)
+{
+	oled_set_cursor(0, 0);
+	if (is_keyboard_left()) 
+	{
+		oled_render_layer_state();
 		oled_render_wpm();
-    } else {
-        oled_render_logo();
-    }
-    return false;
+	} else 
+	{
+		oled_render_logo();
+	}
+	return false;
 }
 #endif // OLED_ENABLE
+
+#ifdef RGB_MATRIX_ENABLE
+
+  // Logical Layout
+  // Columns
+  // Left
+  // 0  1  2  3  4  5
+  //                   ROWS
+  // 25 24 19 18 11 10   0
+  //    03    02    01
+  // 26 23 20 17 12 09   1
+  //    04    05    06
+  // 27 22 21 16 13 08   2
+  //
+  //          15 14 07   3
+  //
+  // Right
+  // 0  1  2  3  4  5
+  //                    ROWS
+  // 25 24 19 18 11 10   4
+  //    03    02    01
+  // 26 23 20 17 12 09   5
+  //    04    05    06
+  // 27 22 21 16 13 08   6
+  //
+  //          15 14 07   7
+  //
+  // Physical Layout
+  // Columns
+  // 0  1  2  3  4  5  6  7  8  9  10 11 12 13
+  //                                           ROWS
+  // 25 24 19 18 11 10       10 11 18 19 24 25  0
+  //    03    02    01       01    02    03
+  // 26 23 20 17 12 09       09 12 17 20 23 26  1
+  //    04                               04
+  // 27 22 21 16 13 08       08 13 16 21 22 27  2
+  //          05    06       06    05
+  //           15 14 07     07 14 15              3
+
+led_config_t g_led_config = { {
+    {  24,  23,  18,  17,  10,   9 },
+    {  25,  22,  19,  16,  11,   8 },
+    {  26,  21,  20,  15,  12,   7 },
+    { NO_LED, NO_LED, NO_LED,  14,  13,   6 },
+    {  51,  50,  45,  44,  37,  36 },
+    {  52,  49,  46,  43,  38,  35 },
+    {  53,  48,  47,  42,  39,  34 },
+    { NO_LED, NO_LED, NO_LED,  41,  40,  33 }
+}, {
+    {  85,  16 }, {  50,  13 }, {  16,  20 }, {  16,  38 }, {  50,  48 }, {  85,  52 }, {  95,  63 },
+    {  85,  39 }, {  85,  21 }, {  85,   4 }, {  68,   2 }, {  68,  19 }, {  68,  37 }, {  80,  58 },
+    {  60,  55 }, {  50,  35 }, {  50,  13 }, {  50,   0 }, {  33,   3 }, {  33,  20 }, {  33,  37 },
+    {  16,  42 }, {  16,  24 }, {  16,   7 }, {   0,   7 }, {   0,  24 }, {   0,  41 }, { 139,  16 },
+    { 174,  13 }, { 208,  20 }, { 208,  38 }, { 174,  48 }, { 139,  52 }, { 129,  63 }, { 139,  39 },
+    { 139,  21 }, { 139,   4 }, { 156,   2 }, { 156,  19 }, { 156,  37 }, { 144,  58 }, { 164,  55 },
+    { 174,  35 }, { 174,  13 }, { 174,   0 }, { 191,   3 }, { 191,  20 }, { 191,  37 }, { 208,  42 },
+    { 208,  24 }, { 208,   7 }, { 224,   7 }, { 224,  24 }, { 224,  41 }
+}, {
+    2, 2, 2, 2, 2, 2, 1,
+    4, 4, 4, 4, 4, 4, 1,
+    1, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 1, 1, 1, 2,
+    2, 2, 2, 2, 2, 1, 4,
+    4, 4, 4, 4, 4, 1, 1,
+    4, 4, 4, 4, 4, 4, 4,
+    4, 4, 1, 1, 1
+} };
+
+#endif //RGB_MATRIX_ENABLE
